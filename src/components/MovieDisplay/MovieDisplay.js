9a14b0983037e532cd1b33f641ef8a6b744837d9
Rae Gebhart <raegebhart@gmail.com>
2023-03-01 13:56:36 -0700

    Format document

--- src/components/MovieDisplay/MovieDisplay.js
+++ src/components/MovieDisplay/MovieDisplay.js
@@ -1,31 +1,30 @@
 import React, { useEffect, useState } from "react";
-import backdrop from '../../assests/spitited-away-background.png'
-import './MovieDisplay.css'
-import { apiCalls } from '../../apiCalls'
+import backdrop from "../../assests/spitited-away-background.png";
+import "./MovieDisplay.css";
+import { apiCalls } from "../../apiCalls";
+import Movie from "../Movie/Movie";
 
 const style = {
-  "backgroundImage": `url(${backdrop})`,
-  "backgroundSize": "cover",
-  "backgroundRepeat": 'no-repeat'
-  
-}
+  backgroundImage: `url(${backdrop})`,
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+};
 
 const MovieDisplay = () => {
-
-  const [movies, setMovies] = useState({})
-
+  const [movies, setMovies] = useState({});
 
   useEffect(() => {
-    setMovies(apiCalls().then(data => console.log(data)))
+    apiCalls().then((data) => setMovies(data));
     console.log(movies)
-  }, [])
-
+    console.log(Object.keys(movies));
+  }, [movies]);
 
   return (
     <section style={style} className="MovieDisplay">
       <section className="center-movie-container"></section>
+      
     </section>
-  )
-}
+  );
+};
 
-export default MovieDisplay
+export default MovieDisplay;
