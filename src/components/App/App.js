2521f14d4f2388a775b59c1459222c20da627fac
Rae Gebhart <raegebhart@gmail.com>
2023-03-01 10:47:20 -0700

    Render Main on homepage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,12 @@
 import { Routes, Route} from 'react-router-dom'
+import Main from '../Main/Main'
 import './App.css';
 
 function App() {
   return (
     <main className="App">
       <Routes>
-        <Route path='/' />
+        <Route path='/' element={<Main />} />
         <Route path='/details/:id' />
         <Route path='*' />
       </Routes>
