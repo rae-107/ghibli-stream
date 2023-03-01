869d798259e9bd0c963a53beead6daa6ce85dd82
Rae Gebhart <raegebhart@gmail.com>
2023-03-01 13:55:42 -0700

    Create Movie component

---
+++ src/components/Movie/Movie.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Movie = () => {
+  return (
+    <section className="Movie" >
+      <img className="small-poster" />
+      <h3 className="small-title" > Title</h3>
+    </section>
+  )
+}
+
+export default Movie
