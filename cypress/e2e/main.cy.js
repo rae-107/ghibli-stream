c490a6e21050121562a2da0e6cd1110beaa91eb2
Rae Gebhart <raegebhart@gmail.com>
2023-03-03 12:22:00 -0700

    Add tests for view

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -1,5 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Main Page', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'https://studio-ghibli-films-api.herokuapp.com/api/', {fixture: 'movies.json'})
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('Should load with the logo', () => {
+    cy.get('.logo').should('be.visible')
+  })
+
+  it('Should have 4 links with different movie views', () => {
+    cy.get('.links > a').should('have.length', 4)  
+  })
+
+  it('Should load with All movies view', () => {
+    cy.get('.links a').first().should('have.class', 'active')
   })
 })
