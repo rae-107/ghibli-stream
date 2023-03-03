726e856a2ff23fa85952d18d8652ffe27e695dd4
Rae Gebhart <raegebhart@gmail.com>
2023-03-03 15:26:04 -0700

    Remove unused useParams

--- src/components/NoMovieDisplay/NoMovieDisplay.js
+++ src/components/NoMovieDisplay/NoMovieDisplay.js
@@ -1,7 +1,6 @@
 import React from "react";
 import backdrop from "../../assests/spirited-away-background.png";
 import "./NoMovieDisplay.css";
-import { useParams } from "react-router-dom";
 
 const style = {
   backgroundImage: `url(${backdrop})`,
@@ -11,7 +10,6 @@ const style = {
 };
 
 const NoMovieDisplay = () => {
-  const { path } = useParams
   return (
     <section className="NoMovieDisplay" style={style}>
       <section className="center-no-movie-display">
