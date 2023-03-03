9c07f7537331cfdad3b9246b64743919fd358506
Rae Gebhart <raegebhart@gmail.com>
2023-03-03 16:51:02 -0700

    Fix tests with changing h tags

--- cypress/e2e/detail.cy.js
+++ cypress/e2e/detail.cy.js
@@ -10,7 +10,7 @@ describe('Detail Page', () => {
   })
 
   it('Should display title', () => {
-    cy.get('h2').contains('Grave of the Fireflies') 
+    cy.get('h1').contains('Grave of the Fireflies') 
   })
 
   it('Should display buttons', () => {
@@ -18,16 +18,16 @@ describe('Detail Page', () => {
   })
 
   it('Should display all movie information',() => {
-    cy.get('p').first().contains('Genre: War | Drama')
-    cy.get('p').eq(1).contains('Release Date: April 16, 1988')
-    cy.get('p').eq(2).contains('Rating: Not Rated')
-    cy.get('p').eq(3).contains('Music: Michio Mamiya')
-    cy.get('p').eq(4).contains('Runtime: 89 mins')
-    cy.get('p').eq(5).contains('Box Office: $516,962')
-    cy.get('p').eq(6).contains('Rotten Tomatoes: 100%')
-    cy.get('p').eq(7).contains('IMDB: 8.5/10')
-    cy.get('p').eq(8).contains('Summary')
-    cy.get('p').eq(9).contains('A devastating meditation on the human cost of war, this animated tale follows Seita (Tsutomu Tatsumi), a teenager charged with the care of his younger sister, Setsuko (Ayano Shiraishi), after an American firebombing during World War II separates the two children from their parents. Their tale of survival is as heartbreaking as it is true to life. The siblings rely completely on each other and struggle against all odds to stay together and stay alive.')
+    cy.get('h2').first().contains('Genre: War | Drama')
+    cy.get('h2').eq(1).contains('Release Date: April 16, 1988')
+    cy.get('h2').eq(2).contains('Rating: Not Rated')
+    cy.get('h2').eq(3).contains('Music: Michio Mamiya')
+    cy.get('h2').eq(4).contains('Runtime: 89 mins')
+    cy.get('h2').eq(5).contains('Box Office: $516,962')
+    cy.get('h2').eq(6).contains('Rotten Tomatoes: 100%')
+    cy.get('h2').eq(7).contains('IMDB: 8.5/10')
+    cy.get('h2').eq(8).contains('Summary')
+    cy.get('h2').eq(9).contains('A devastating meditation on the human cost of war, this animated tale follows Seita (Tsutomu Tatsumi), a teenager charged with the care of his younger sister, Setsuko (Ayano Shiraishi), after an American firebombing during World War II separates the two children from their parents. Their tale of survival is as heartbreaking as it is true to life. The siblings rely completely on each other and struggle against all odds to stay together and stay alive.')
   })
 
   it('Should be able to get to main view by clicking on logo', () => {
