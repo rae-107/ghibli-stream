b6a3312762438385027de0c3b3d72245f2fa0bcf
Rae Gebhart <raegebhart@gmail.com>
2023-03-02 19:02:42 -0700

    Add image with arrow for link decoration

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,17 +2,20 @@ import React from "react";
 import logo from "../../assests/Studio_Ghibli_logo.png";
 import "./Header.css";
 import { NavLink } from "react-router-dom";
+import arrow from '../../assests/angle-small-down.png'
 
 const Header = () => {
   return (
     <header className="Header">
-      <NavLink to={'/'} >
+      <NavLink to={"/"}>
         <img className="logo" src={logo} />
       </NavLink>
-      <NavLink to={'/'}>All Movies</NavLink>
-      <NavLink to={'/favorites'}>Favorites</NavLink>
-      <NavLink to={'/watched'}>Watched</NavLink>
-      <NavLink to={'/watchlist'}>Watch List</NavLink>
+      <div className="links" >
+        <NavLink to={"/"}><img className="arrow" src={arrow} />All Movies</NavLink>
+        <NavLink to={"/favorites"}><img className="arrow" src={arrow} />Favorites</NavLink>
+        <NavLink to={"/watched"}><img className="arrow" src={arrow} />Watched</NavLink>
+        <NavLink to={"/watchlist"}><img className="arrow" src={arrow} />Watch List</NavLink>
+      </div>
     </header>
   );
 };
